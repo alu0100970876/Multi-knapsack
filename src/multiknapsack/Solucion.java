8d60154e53e18c1f0520287842317ae1eb09f8dd
AlejandroLF <alu0100973792@ull.edu.es>
2018-04-22 10:26:22 +0100

    solucion estructura

--- src/multiknapsack/Solucion.java
+++ src/multiknapsack/Solucion.java
@@ -1,5 +1,26 @@
 package multiknapsack;
 
 public class Solucion {
-
+	static int capacidades[];
+	static double beneficios[];
+	int solucion[];
+	
+	public static int getCapacidad(int index) {
+		return capacidades[index];
+	}
+	public static void setCapacidad(int index, int capacidad) {
+		Solucion.capacidades[index] = capacidad;
+	}
+	public static double getBeneficio(int index) {
+		return beneficios[index];
+	}
+	public static void setBeneficios(int index, double beneficio) {
+		Solucion.beneficios[index] = beneficio;
+	}
+	public int getSolucion(int index) {
+		return solucion[index];
+	}
+	public void setSolucion(int index, int mochila) {
+		this.solucion[index] = mochila;
+	}
 }
