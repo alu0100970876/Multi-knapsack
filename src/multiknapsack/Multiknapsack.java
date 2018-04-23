fcaafa05cb317912216289883f2bca11f528fc24
Miguel Jiménez Gomis <alu0100970876@ull.edu.es>
2018-04-23 18:05:01 +0100

    GRASP movimiento 1 aleatorio

--- src/multiknapsack/Multiknapsack.java
+++ src/multiknapsack/Multiknapsack.java
@@ -4,10 +4,15 @@ import java.util.ArrayList;
 
 public class Multiknapsack {
 	public static void main(String args[]) {
-		Solucion.readFromFile("problema1.txt");
-		Solucion sol1 = new Solucion();
-		System.out.println(sol1);
+		Solucion sol1 = new Solucion("problema1.txt");
+		System.out.println("Solucion 1 inicial: " + sol1+ " Valor: " + sol1.valorTotal());
 		sol1.greedySolve();
-		System.out.println(sol1);
+		System.out.println("Solucion 1 final: " + sol1 + " Valor: " + sol1.valorTotal());
+		System.out.println("FIN");
+		Solucion sol2 = new Solucion("problema1.txt");
+		System.out.println("Solucion 2 inicial: " + sol2+ " Valor: " + sol2.valorTotal());
+		sol2.GRASP(100000);
+		System.out.println("Solucion 2 final: " + sol2 + " Valor: " + sol2.valorTotal());
+		System.out.println("FIN");
 	}
 }
--- src/multiknapsack/Solucion.java
+++ src/multiknapsack/Solucion.java
@@ -4,22 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Solucion {	
-	static ArrayList<Integer> capacidades = new ArrayList<Integer>();
-	static ArrayList<Integer> beneficios = new ArrayList<Integer>();
-	static ArrayList<Integer> pesos = new ArrayList<Integer>();
-	ArrayList<Integer> solucion = new ArrayList<Integer>();
+	ArrayList<Integer> capacidades = new ArrayList<Integer>();
+	ArrayList<Integer> beneficios = new ArrayList<Integer>();
+	ArrayList<Integer> pesos = new ArrayList<Integer>();
+	public ArrayList<Integer> solucion = new ArrayList<Integer>();
 	int valortotal;
 	
-	Solucion() {
+	public Solucion(String filename) {
+		readFromFile(filename);
 		solucion = new ArrayList<Integer>();
 		for(int i = 0; i < beneficios.size(); ++i)
 			solucion.add(-1);
 		setValortotal(0);
 	}
 	
-	static void readFromFile(String inputfile) {
+	public Solucion(Solucion other) {
+		capacidades = other.capacidades;
+		beneficios = other.beneficios;
+		pesos =  other.pesos;
+		solucion = other.solucion;
+	}
+	
+	
+	public void readFromFile(String inputfile) {
 		BufferedReader reader = null;
 		try {
 			reader = new BufferedReader(new FileReader(inputfile));
@@ -56,28 +66,28 @@ public class Solucion {
 		this.valortotal = valortotal;
 	}
 
-	public static int getCapacidad(int index) {
+	public  int getCapacidad(int index) {
 		return capacidades.get(index);
 	}
 	
-	public static void setCapacidad(int index, int capacidad) {
-		Solucion.capacidades.set(index, capacidad);
+	public  void setCapacidad(int index, int capacidad) {
+		capacidades.set(index, capacidad);
 	}
 	
-	public static int getBeneficio(int index) {
+	public  int getBeneficio(int index) {
 		return beneficios.get(index);
 	}
 	
-	public static void setBeneficio(int index, int beneficio) {
-		Solucion.beneficios.set(index, beneficio);
+	public  void setBeneficio(int index, int beneficio) {
+		beneficios.set(index, beneficio);
 	}
 	
-	public static int getPeso(int index) {
+	public  int getPeso(int index) {
 		return pesos.get(index);
 	}
 
-	public static void setPeso(int index, int peso) {
-		Solucion.pesos.set(index, peso);
+	public  void setPeso(int index, int peso) {
+		pesos.set(index, peso);
 	}
 
 	public int getSolucion(int index) {
@@ -145,6 +155,7 @@ public class Solucion {
 		return true;
 	}
 	
+	
 	public void greedySolve() {
 		ArrayList<Integer> beneficiosRestantes = (ArrayList<Integer>) beneficios.clone();
 		ArrayList<Integer> pesosRestantes = (ArrayList<Integer>) pesos.clone();
@@ -170,8 +181,7 @@ public class Solucion {
 					v[i][j] = Math.max(left, up);
 				}
 			}
-		}
-		
+		}		
 		// Obtener indices de los objetos metidos en esta mochila
 		// Indices relativos a los objetos restantes
 		ArrayList<Integer> indicesRelativos = new ArrayList<Integer>();
@@ -182,8 +192,7 @@ public class Solucion {
 				indicesRelativos.add(i-1);
 				capacidadRestante = capacidadRestante - getPeso(i-1);
 			}
-		}
-		
+		}		
 		// Actualizar la solucion
 		// i -> recorrer solucion general
 		// j -> recorrer indices relativos (van de mayor a menor)
@@ -198,8 +207,7 @@ public class Solucion {
 				}
 				k--;
 			}
-		}
-		
+		}		
 		// Borrar los objetos metidos de los objetos restantes
 		for(int i = 0; i < indicesRelativos.size(); i++) {
 			// Cast a int para que lo tome como indice, en lugar de elemento, a borrar
@@ -209,11 +217,82 @@ public class Solucion {
 	}
 	
 	public String toString() {
-		String cadena = "";
-		
+		String cadena = "";		
 		for(int i = 0; i < solucion.size(); ++i)
-			cadena += getSolucion(i) + " ";
-			
+			cadena += getSolucion(i) + " ";			
 		return cadena;
+	}
+	
+	/** Resolucion del problema con aproximación grasp
+	 * @param numOfIterations
+	 */
+	public void GRASP(int numOfIterations) {
+		ArrayList<Integer> mejor = solucion;
+		for(int i = 0; i < numOfIterations; i++) {
+			Solucion temp = new Solucion(this); // se genera una solucion identica y se limpia
+			for(int j = 0; j < temp.solucion.size(); j++) {
+				temp.solucion.set(j, -1);
+			}
+			temp.generarSolucion(this.solucion.size() / 2); // se genera una solucion aleatoria
+			//System.out.println("Solucion generada:" + temp + " valor: " + temp.valorTotal());
+			temp.mov1(true); // se realiza una busqueda local para esa solucion con uno de los movimientos
+			if(this.valorTotal() < temp.valorTotal()) {
+				this.solucion  = new ArrayList<Integer>(temp.solucion); // si la nueva solucion es mejor, se cambia
+			}
+		}
+	}
+	
+	/** Movimiento posible 1, quitar elementos delas mochilas e introducir otros. version aleatoria y greede
+	 * @todo version greedy
+	 * @param type
+	 */
+	public void mov1( boolean type) {
+		if(type) { // versión aleatoria de este movimiento
+			Random rand = new Random();
+			ArrayList<Integer> posiblesentran =  new ArrayList<Integer>();
+			for(int i = 0; i < this.solucion.size(); i++) { // lista de posibles indices a entrar
+				if(this.solucion.get(i) == -1) {
+					posiblesentran.add(i);
+				}
+			}
+			for(int i = 0; i < posiblesentran.size(); i++) { // para un numero determinado de veces (parametro) se introduce una de las posibilidades en una mochila aleatoria
+				int entra = rand.nextInt( posiblesentran.size()); // elemento que entra
+				int mochila =  rand.nextInt(this.capacidades.size());// mochila aleatoria
+				this.solucion.set(entra, mochila);
+				posiblesentran.remove(entra);
+				while(!isValid()) {// si la solucion no es valida se eliminan objetos y se ponen en la lista de posibles a entrar
+					ArrayList<Integer> posiblessalir =  new ArrayList<Integer>();
+					for(int j = 0; j < this.solucion.size(); j++) {// los posibles a salir son los que esten en esa mochila
+						if(this.solucion.get(i) == mochila) {
+							posiblessalir.add(i);
+						}
+					}
+					if(posiblessalir.size() == 0) {
+						break;
+					}
+					int sale = rand.nextInt(posiblessalir.size());
+					this.solucion.set(sale, -1); // se saca un elemento aleatorio de dicha mochila
+				}
+			}
+		}
+	}
+	
+	/** Genera una solucion aleatoria
+	 * @param tamanio
+	 */
+	public void generarSolucion(int tamanio) {
+		Random rand = new Random();
+		ArrayList<Integer> posibles =  new ArrayList<Integer>();
+		for(int i = 0; i < this.solucion.size(); i++) {
+			posibles.add(i);
+		}	
+		for(int i = 0; i < tamanio; i++) {
+			int entra = rand.nextInt( this.solucion.size() - 1); // se selecciona un objeto aleatorio (su indice) que referira a los posibles
+			this.solucion.set(posibles.get(entra), rand.nextInt( this.capacidades.size()));// el objeto seleccionado entra en una mochila aleatoria
+			posibles.remove(entra);// se saca esa posibilidad para que no se repita
+			if(!this.isValid()) {
+				this.solucion.set(entra, -1);// si al introducir la solucion no es valida, se saca
+			}
+		}		
 	}
 }
